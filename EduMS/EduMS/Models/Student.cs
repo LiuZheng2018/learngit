63cf0df2ed04625115a92043070725df995eacfc
LiuZheng <ljllzq@qq.com>
2018-03-22 14:42:41 +0800

    新建项目_学生类

---
+++ EduMS/EduMS/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduMS.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name ="学号")]
+        public int S_Id { get; set; }
+        [Display(Name = "姓名")]
+        public string S_Name { get; set; }
+        [Display (Name ="性别")]
+        public string S_Sex { get; set; }
+        [Display (Name ="年龄")]
+        public int S_Age { get; set; }
+
+        public virtual ICollection <Course> Courses { get; set; }
+
+
+    }
+}
