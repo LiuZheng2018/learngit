7f3111a68b7bcdfec526d754022169e8a96a6d3f
LiuZheng <ljllzq@qq.com>
2018-03-26 16:03:44 +0800

    Model设计&Codefirst建立MySQL数据库

---
+++ SercicesMS/SercicesMS/Migrations/201803260758199_Service.cs
@@ -0,0 +1,31 @@
+namespace SercicesMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Service : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        S_Name = c.String(nullable: false, unicode: false),
+                        S_Path = c.String(nullable: false, unicode: false),
+                        Pic_Path = c.String(unicode: false),
+                        S_User = c.String(unicode: false),
+                        S_Pwd = c.Int(nullable: false),
+                        S_CreatTime = c.String(unicode: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Services");
+        }
+    }
+}
---
+++ SercicesMS/SercicesMS/Models/ServiceDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace SercicesMS.Models
+{
+    public class ServiceDBContext:DbContext
+    {
+        static ServiceDBContext()
+        {
+            DbConfiguration.SetConfiguration(new MySql.Data.Entity.MySqlEFConfiguration());
+        }
+        public DbSet<Service> Services { get; set; }
+    }
+}
