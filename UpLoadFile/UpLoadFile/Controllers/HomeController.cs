2ba3efa6e8733d3db81076fa3d9035066660c11f
LiuZheng <ljllzq@qq.com>
2018-03-30 14:59:52 +0800

    图片上传和图片预览

---
+++ UpLoadFile/UpLoadFile/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace UpLoadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            var fileName = file.FileName;
+            var filePath = Server.MapPath(string.Format("~/{0}","File"));
+            string s = Path.Combine(filePath, fileName).ToString();
+            file.SaveAs(s);
+            return View();
+        }
+    }
+}
